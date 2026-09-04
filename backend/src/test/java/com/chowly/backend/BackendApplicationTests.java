61b0deebb26f6d9fc9017ac99eae586d172773a9
Oluwaseun Bamigbele <sbamigbele10@gmail.com>
2026-09-04 21:13:33 +0100

    chore: initialize Chowly backend

---
+++ backend/src/test/java/com/chowly/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chowly.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
